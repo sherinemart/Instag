7c40fbb5eaabcb00d4a062676b7774e06cc2fccd
sherinemart <91425378+sherinemart@users.noreply.github.com>
2022-03-18 19:56:30 +0530

    Initial commit

---
+++ Arith.java
@@ -0,0 +1,17 @@
+class Arith
+{
+ public static void main(String args[])
+ {
+  int a=54;
+  int b=0;
+try
+{
+  int c=a/b;
+}catch(ArithmeticException e)
+{
+   System.out.println(" ");
+}
+  System.out.println("Rest of the code");
+
+}
+}
